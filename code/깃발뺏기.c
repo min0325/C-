55901f2b1aee360267851310784f9ef95196e274
Unknown <chamin0325@naver.com>
2018-03-11 14:10:24 +0900

    깃발뺏기

---
+++ code/깃발뺏기.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct route_info  //경로 정보 
+{
+	int go_num;  //출발 성번호 
+	int arrival_num  //도착 성번호 
+	int knight;  //필요한 병사 수 
+}route_info;
+
+void 
+
+int main()
+{
+	int num;
+	printf("성 개수 입력 : ");
+	scanf("%d", &num);
+	for(i=0;i<num;i++)
+	{
+		for(j=0;j,num;j++)
+		{
+			route[i][j] = 0;
+			printf("%3d", route[i][j]);
+		}
+	printf("\n");
+	}
+	route-info r_info[route_num]//
+	INPUT_ROUTE_INFO(&r_info); //함수 실행 
+	
+	//M이 경로 수라면 M번 만큼 함수 실행 
+	return 0;
+}
+
